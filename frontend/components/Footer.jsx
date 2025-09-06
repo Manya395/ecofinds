3675a584a246e456084b014e2c6da55ba1230d88
Manya <manyagowda395@gmail.com>
2025-09-06 13:48:33 +0530

    Delete frontend directory

--- frontend/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="bg-gray-800 text-white text-center py-4 mt-6">
-      <p>© 2025 EcoFinds – Sustainable Second-Hand Marketplace</p>
-    </footer>
-  );
-}
-
-export default Footer;
